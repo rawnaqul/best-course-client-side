92e2240b7d4c21b592ffea6b0a50fdb44b8b2a5f
rawnaqul <slow.coach.rawnaq@gmail.com>
2022-10-25 23:44:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
+import { FaBeer } from 'react-icons/fa';
+import toast, { Toaster } from 'react-hot-toast';
 import './App.css';
 
 function App() {
+  const notify = () => toast('Here is your toast.');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
