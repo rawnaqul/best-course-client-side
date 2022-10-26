b93c74ba8c09395177d602abe9dfac0ce2b2322e
rawnaqul <slow.coach.rawnaq@gmail.com>
2022-10-26 15:36:26 +0600

    new update

--- src/pages/courses/Allcourses.js
+++ src/pages/courses/Allcourses.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Courses from './Courses';
+import Sidebar from './Sidebar';
 
 const Allcourses = () => {
     return (
         <div>
-            all courses are here
+            <div className='grid grid-cols-4 gap-4'>
+                <div className='col-span-1 bg-white'>
+                    <Sidebar></Sidebar>
+                </div>
+                <div className='col-span-3 bg-gray-50'>
+                    <Courses></Courses>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/pages/courses/CourseDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CourseDetails = () => {
+    return (
+        <div>
+            details about courses
+        </div>
+    );
+};
+
+export default CourseDetails;
